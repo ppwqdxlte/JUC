b6e4f3772e345cd692a80b2891f6aff9b5a920b5
ppwqdxlte <rexinwangmin@foxmail.com>
2022-01-19 19:51:27 +0800

    ReentrantLock供过于求、供不应求的问题
    供过于求：producer生产超过consumer消费时，程序能正常结束;
    供不应求：程序不能正常结束，阻塞在consumerCondition这里，我也不知道为啥，以及怎么解决这个BUG！！

--- src/com/laowang/test/T06_MyContrainer2.java
+++ src/com/laowang/test/T06_MyContrainer2.java
@@ -1,6 +1,7 @@
 package com.laowang.test;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,22 +9,36 @@ public class T06_MyContrainer2<T> {
 
     private final LinkedList<T> list = new LinkedList<>();
     private final int MAX = 10;
-    private int count;
 
     private final ReentrantLock lock = new ReentrantLock();
     private final Condition producer = lock.newCondition();
     private final Condition consumer = lock.newCondition();
 
-    public void add(T t){
+    public void add(T t) {
+        try {
+            lock.lock();//上锁
+            while (list.size() == MAX){
+                producer.await();//生产者阻塞
+            }
+            list.add(t);
+            System.out.println(Thread.currentThread().getName()+" 生成一个，共有商品 "+ list.size());
+            consumer.signalAll();//通知消费者
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();//开锁
+        }
+    }
+
+    public void get() {
         try {
             lock.lock();
-            while (count == MAX){
-                producer.await();
+            while (list.size() == 0){
+                consumer.await(3, TimeUnit.SECONDS);//阻塞消费者
             }
-            ++count;
-            list.add(t);
-            System.out.println(Thread.currentThread().getName()+" 生成一个，共有商品 "+ count);
-            consumer.signalAll();
+            T t = list.pop();
+            System.out.println(Thread.currentThread().getName()+" 消费一个，还剩商品 "+ list.size());
+            producer.signalAll();//通知所有生产者快点生产
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -31,47 +46,20 @@ public class T06_MyContrainer2<T> {
         }
     }
 
-    public void get(){
-        try {
-            lock.lock();
-            while (count == 0){
-                consumer.await();
-            }
-            --count;
-            T t = list.pop();
-            System.out.println(Thread.currentThread().getName()+" 消费一个，还剩商品 "+ count);
-            producer.signalAll();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-    }
-
     public static void main(String[] args) {
-        T06_MyContrainer2 test = new T06_MyContrainer2();
+        T06_MyContrainer2<Object> test = new T06_MyContrainer2<>();
 
         for (int i = 0; i < 2; i++) {
             new Thread(()->{
-                while (true){
+                for (int j = 0; j < 20; j++) {//每个producer最多生产20个
                     test.add(new Object());
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
             },"producer"+i).start();
         }
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
-                while (true){
+                for (int j = 0; j < 5; j++) {//每个consumer最多消费5个
                     test.get();
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
             },"consumer"+i).start();
         }
