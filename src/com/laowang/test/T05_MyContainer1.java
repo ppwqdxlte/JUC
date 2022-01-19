972ce85652a2edcd3e8992f17d661609f00e0419
ppwqdxlte <rexinwangmin@foxmail.com>
2022-01-19 16:00:28 +0800

    对上个题目代码的优化：去掉不必要的sleep，添加while确保通知到位，添加超时抛出异常终止线程的功能

--- src/com/laowang/test/T05_MyContainer1.java
+++ src/com/laowang/test/T05_MyContainer1.java
@@ -1,6 +1,7 @@
 package com.laowang.test;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeoutException;
 
 /*
 * 【题目】写一个容器，生产者消费者模式，用wait()或notify()或notifyAll()实现 2个生产者和若干个消费者
@@ -13,32 +14,41 @@ public class T05_MyContainer1<T> {
 
     private int count;
 
-    public synchronized void add(T t){
-        if (count < MAX){
-            list.push(t);
-            ++count;
-            System.out.println(Thread.currentThread().getName()+" 制造了一个，目前库存为"+count);
-            this.notifyAll();
-        }else{
+    public synchronized void add(T t) throws TimeoutException {
+        long startUp = System.currentTimeMillis();
+        while (count == MAX){ //删掉停顿时间后，if-else有问题，所以改成了while
+            if (System.currentTimeMillis()-startUp >= 10000){
+                this.notifyAll();//通知consumer，防止阻塞
+                throw new TimeoutException("工厂"+Thread.currentThread().getName()+"等待超时，停止生产");
+            }
             try {
+                this.notifyAll();
                 this.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        list.push(t);
+        ++count;
+        System.out.println(Thread.currentThread().getName()+" 制造了一个，目前库存为"+count);
+        this.notifyAll();
     }
-    public synchronized T get(){
-        if (count == 0){
+    public synchronized T get() throws TimeoutException {
+        long startUp = System.currentTimeMillis();
+        while (count == 0){
+            if (System.currentTimeMillis() - startUp >= 10000){
+                this.notifyAll();//通知其它线程尤其producer，防止阻塞
+                throw new TimeoutException("消费者"+Thread.currentThread().getName()+"等待超时，取消订单");
+            }
             try {
                 this.notify();//通知厂家赶紧造
                 this.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }else {
-            --count;
-            System.out.println(Thread.currentThread().getName()+"取走了一个，目前库存为"+count);
         }
+        --count;
+        System.out.println(Thread.currentThread().getName()+"取走了一个，目前库存为"+count);
         return list.poll();
     }
 
@@ -47,12 +57,12 @@ public class T05_MyContainer1<T> {
 
         for (int i = 0; i < 2; i++) {
             new Thread(()->{
-                while (true){
-                    container1.add(new Object());
+                for (int j = 0; j < 25; j++) {
                     try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        container1.add(new Object());
+                    } catch (TimeoutException e) {
+                        System.out.println(e.getMessage());
+                        break;
                     }
                 }
             },"producer"+i).start();
@@ -60,12 +70,12 @@ public class T05_MyContainer1<T> {
 
         for (int i = 0; i < 6; i++) {
             new Thread(()->{
-                while (true){
-                    container1.get();
+                for (int j = 0; j < 5; j++) {
                     try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        container1.get();
+                    } catch (TimeoutException e) {
+                        System.out.println(e.getMessage());
+                        break;
                     }
                 }
             },"consumer"+i).start();
